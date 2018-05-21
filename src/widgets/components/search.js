4b8ee0a9e539f46577341ec904801026f30c5317
Johnny IV Young O <asesordeprogramacion@gmail.com>
2018-05-21 14:56:49 -0500

    [feat] first implementation of the Search

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './search.css'
 
-const Search = () => (
-	<form action="" className="Search">
+const Search = (props) => (
+	<form onSubmit={props.handleSubmit} className="Search">
 		<input 
+			ref={props.setRef}
 			type="text"
 			placeholder="Busca tu video favorito"
-			className="Search-input" />
+			className="Search-input"
+			name="search"
+			onChange={props.handleChange}
+			value={props.value} />
 	</form>
 );
 export default Search;
